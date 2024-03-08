3b1e64501530a4998203e7f44a0d41801e816f33
“Vinaye” <vinaye222@gmail.com>
2024-03-08 00:20:18 -0500

    Implement ViewModel And Repository

---
+++ app/src/main/java/com/vinayeapps/dependencyinjectionkoin/data/MainRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.vinayeapps.dependencyinjectionkoin.data
+
+
+/**
+ * Created by vinaye on 2024-03-07.
+ * Author: Vinay Sebastian
+ */
+class MainRepositoryImpl:MainRepository
+{
+    override fun doNetworkCall() {
+        super.doNetworkCall()
+    }
+}
